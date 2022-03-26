4d1e34ed7da17a4206fb4d1e61cc9ed55685ad43
Lucas Lanzone <lucaslanzone@gmail.com>
2022-03-26 09:25:20 +0100

    proyecto iniciado

--- appcoder/views.py
+++ appcoder/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request, "appcoder/index.html")
+
+def estudiantes(request):
+    return HttpResponse("vista de estudiantes")
+
+def profesores(request):
+    return HttpResponse("vista de profesores")
+
+def cursos(request):
+    return HttpResponse("vista de cursos")
+
+def entregables(request):
+    return HttpResponse("vista de entregables")
